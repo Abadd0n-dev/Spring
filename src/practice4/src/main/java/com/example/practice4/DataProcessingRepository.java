1a180bba7861633a9a4fd7818f946a4768ac3dad
[Ruslan] <[rkutaraev@list.ru]>
2024-02-01 12:47:31 +0300

    code refactoring 4

---
+++ src/practice4/src/main/java/com/example/practice4/DataProcessingRepository.java
@@ -0,0 +1,8 @@
+package com.example.practice4;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface DataProcessingRepository extends JpaRepository<User,Long> {
+
+}
