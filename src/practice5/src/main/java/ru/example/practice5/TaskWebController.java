43be5670f555401892c1b9c172742379308591e5
[Ruslan] <[rkutaraev@list.ru]>
2024-02-04 15:19:58 +0300

    code refactoring 5

---
+++ src/practice5/src/main/java/ru/example/practice5/TaskWebController.java
@@ -0,0 +1,21 @@
+package ru.example.practice5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web/tasks")
+public class TaskWebController {
+    private final TaskService service;
+    @Autowired
+    public TaskWebController(TaskService service) {
+        this.service = service;
+    }
+    @RequestMapping()
+    public String tasksPage(Model model) {
+        model.addAttribute("tasks", service.getTasks());
+        return "tasks";
+    }
+}
